b4dfe64a60ab53ff427403629a36b18d748d149c
Gingene <ginx2618@gmail.com>
2024-02-25 00:18:41 +0800

    Updates

--- assets/chevrons-right-x4bVvvVY.js
+++ assets/chevrons-right-x4bVvvVY.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-DW8hoVIS.js";/**
+import{d as e}from"./index-lmB3NvbC.js";/**
  * @license lucide-vue-next v0.323.0 - ISC
  *
  * This source code is licensed under the ISC license.
